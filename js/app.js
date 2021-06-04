2b4d779bd81e9d3722f16b683f920230bef20fac
cykic <cyb3rcykic@gmail.com>
2021-06-03 22:42:08 -0400

    rounding up cart integration

--- js/app.js
+++ js/app.js
@@ -2,37 +2,76 @@ import { Cart } from "./cart.js";
 import { warning } from "./helper.js";
 import { Payment } from "./payment.js";
 
-const testButtons = document.querySelectorAll("#test");
 const checkout = document.querySelector(".checkout-btn");
+const cartButton = document.querySelector("#cart");
+const cartview = document.querySelector("#cart-view");
+const mainView = document.querySelector(".main");
+
+// click to show cart
+cartButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  let id = e.target.id;
+  document.title = id;
+  window.history.pushState({ id }, `${id}`, `/${id}`);
+  mainView.classList.add("hide");
+  cartview.classList.remove("hide");
+});
 
 // cart
 export const cart = new Cart();
 async function initcart() {
-  cart.retrievefromLocalStorage()
-  cart._updateCart()
+  window.addEventListener("popstate", (event) => {
+    mainView.classList.remove("hide");
+    cartview.classList.add("hide");
+  });
+  // HIDE CART VIEW
+  cartview.classList.add("hide");
+  //
+  cart.retrievefromLocalStorage();
+  cart._updateCart();
 
   // CHECKOUT BUTTON
   checkout.addEventListener("click", function () {
-    if(cart.totalAmt === 0){
-      warning("Add Item to Cart to checkout")
-      checkout.classList.add('disabled')
-      return
+    if (cart.totalAmt === 0) {
+      warning("Add Item to Cart to checkout");
+      checkout.classList.add("disabled");
+      return;
     }
-    checkout.classList.remove('disabled')
+    checkout.classList.remove("disabled");
     new Payment().makePayment(cart.totalAmt);
   });
 }
 
 initcart();
 
-testButtons.forEach(button =>{
-  button.addEventListener('click',function(){
-    cart.addtoCart(`${button.dataset.id}`)
-  })
-})
+export function getAddToCartButtons() {
+  const targetNode = document.body;
+  const config = { childList: true, subtree: true };
+
+  const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        console.log("DOM changed");
+
+        const addToCarts = document.querySelectorAll(".add-to-cart");
+        addToCarts.forEach((button) => {
+          button.addEventListener("click", click);
+        });
+      }
+    }
+  };
+
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+}
+
+const click = (e) => {
+  console.log(e.target);
+  cart.addtoCart(`${e.target.dataset.id}`);
+};
 
 // testButton.addEventListener("click", async function () {
 //   // await cart.getAllProducts();
 //   cart.addtoCart(`${testButton.dataset.id}`);
-  
+
 // });
--- js/controller.js
+++ js/controller.js
@@ -1,93 +1,99 @@
-import loginView from '../views/loginView.js'
-import registerView from '../views/registerView.js'
-import * as model from './model.js'
-import category1View from '../views/category1View.js'
-import category2View from '../views/category2View.js'
-import categoryView from '../views/categoryView.js'
-import currentCategoryView from '../views/currentCategoryView.js'
-import singleProductView from '../views/singleProductView.js'
-import abstractView from '../views/abstractView.js'
-import cartView from '../views/cartView.js'
+import loginView from "../views/loginView.js";
+import registerView from "../views/registerView.js";
+import * as model from "./model.js";
+import category1View from "../views/category1View.js";
+import category2View from "../views/category2View.js";
+import categoryView from "../views/categoryView.js";
+import currentCategoryView from "../views/currentCategoryView.js";
+import singleProductView from "../views/singleProductView.js";
+import abstractView from "../views/abstractView.js";
+import cartView from "../views/cartView.js";
+import { getAddToCartButtons } from "./app.js";
 
-const abstract= new abstractView()
-function push(event, view){
-  let id= event.target.id; 
-  if(!view) return
-  view.showViews()
-   document.title=id 
-   window.history.pushState({id}, `${id}`, `/${id}`)
+const abstract = new abstractView();
+function push(event, view) {
+  let id = event.target.id;
+  if(!id)id =''
+  document.title = id;
+  window.history.pushState({ id }, `${id}`, `/${id}`);
+  if (!view) return;
+  view.showViews();
 }
 
-window.onload = event=>{
-    window['registerView'].addEventListener('click', event=> push(event, registerView))
-    window['loginView'].addEventListener('click', event=> push(event, loginView))
-    window['cartView'].addEventListener('click', event=> push(event, cartView))
-   
+window.onload = (event) => {
+  window["registerView"].addEventListener("click", (event) =>
+    push(event, registerView)
+  );
+  window["loginView"].addEventListener("click", (event) =>
+    push(event, loginView)
+  );
+  
+};
+window.addEventListener("popstate", (event) => {
+  push(event, abstract.defaultView());
+});
+
+loginView.getInputValues();
+registerView.registerRedirect();
+loginView.loginRedirect();
+loginView.removeLogin();
+category1View.renderSpinner();
+
+async function registerHandler() {
+  try {
+    registerView.getInputValues();
+    registerView.removeRegistration();
+  } catch (err) {
+    registerView.renderError(err);
+  }
 }
-window.addEventListener('popstate', event=> {
-   push(event, abstract.defaultView())
+registerHandler();
 
-})
+async function controlCategoriesView() {
+  try {
+    await model.getProducts();
+    await model.getCategories();
+    category1View.showViews(model.state.category1);
+    category1View.seeMoreHandler(model.state.category1All);
+    category2View.seeMoreHandler2(model.state.category2All);
+    category2View.showViews(model.state.category2);
+    
 
-loginView.getInputValues()
-registerView.registerRedirect()
-loginView.loginRedirect()
-loginView.removeLogin()
-category1View.renderSpinner()
-async function registerHandler(){
-    try{
-        registerView.getInputValues()
-        registerView.removeRegistration()
-    }
-    catch(err){
-        registerView.renderError(err)
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
-registerHandler()
-
-async function controlCategoriesView(){
-    try{  
-      await model.getProducts()
-      await model.getCategories()
-        category1View.showViews(model.state.category1)
-        category1View.seeMoreHandler(model.state.category1All)
-        category2View.seeMoreHandler2(model.state.category2All)
-        category2View.showViews(model.state.category2)
-    }
-    catch(err){
-        console.log(err);
-        
-    }
-}  
-async function controlCategories(){
-   
-     try{
-await model.getCategories()
-categoryView.showViews(model.state.categories)
- }
- catch(err){
-     console.log(err);
-     categoryView.renderProductsError(err)
- }
+async function controlCategories() {
+  try {
+    await model.getCategories();
+    categoryView.showViews(model.state.categories);
+    getAddToCartButtons()
+  } catch (err) {
+    console.log(err);
+    categoryView.renderProductsError(err);
+  }
 }
 
-
-async function controlDisplayCategories(categoryName){
-    if(!categoryName) return;
-    model.displayCategories(categoryName)
-    currentCategoryView.showViews(model.state.currentCategory)
+async function controlDisplayCategories(categoryName) {
+  if (!categoryName) return;
+  model.displayCategories(categoryName);
+  currentCategoryView.showViews(model.state.currentCategory);
+  
 }
-async function controlSingleProducts(){
-    const id = window.location.hash.slice(1)
-    if(!id) return;
-  await  model.getSingleProduct(id)
-    singleProductView.showViews(model.state.singleProduct)
+async function controlSingleProducts() {
+  const id = window.location.hash.slice(1);
+  if (!id) return;
+  await model.getSingleProduct(id);
+  singleProductView.showViews(model.state.singleProduct);
+  
 }
-categoryView.curCategoryViewHandler(controlDisplayCategories)
+categoryView.curCategoryViewHandler(controlDisplayCategories);
 
- window.addEventListener('load', controlCategoriesView)
- 
- window.addEventListener('load', controlCategories)
+window.addEventListener("load", controlCategoriesView);
 
- window.addEventListener('hashchange', controlSingleProducts)
+window.addEventListener("load", controlCategories);
+
+window.addEventListener("hashchange", controlSingleProducts);
+
+
 
